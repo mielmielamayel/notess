6eed4f5710a080c0a67a3ef05ef879f1a5b0f4f8
mielmielamayel <143404324+mielmielamayel@users.noreply.github.com>
2023-09-19 22:20:25 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,100 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const addNoteButton = document.getElementById("add-note");
+    const saveNotesButton = document.getElementById("save-notes");
+    const noteContainer = document.getElementById("note-container");
+    let notesArray = []; 
+
+    loadNotes();
+
+    addNoteButton.addEventListener("click", createNote);
+    saveNotesButton.addEventListener("click", saveNotes);
+
+    function createNote() {
+        const note = document.createElement("div");
+        note.className = "note";
+
+        const textarea = document.createElement("textarea");
+        textarea.className = "note-textarea";
+        textarea.placeholder = "Write your note here...";
+
+        const deleteButton = document.createElement("button");
+        deleteButton.className = "delete-button";
+        deleteButton.innerText = "Delete";
+
+        deleteButton.addEventListener("click", () => {
+            noteContainer.removeChild(note);
+         
+            removeNoteFromArray(note);
+      
+            removeNoteFromLocalStorage(textarea);
+        });
+
+      
+        noteContainer.insertBefore(note, noteContainer.firstChild);
+
+        note.appendChild(textarea);
+        note.appendChild(deleteButton);
+
+        notesArray.unshift({ textarea, note });
+    }
+
+    function saveNotes() {
+        
+        notesArray = notesArray.filter(noteData => noteContainer.contains(noteData.note));
+        
+        localStorage.setItem("notes", JSON.stringify(notesArray.map(noteData => noteData.textarea.value)));
+    }
+
+    function loadNotes() {
+        const notesData = JSON.parse(localStorage.getItem("notes"));
+        if (Array.isArray(notesData)) {
+            
+            noteContainer.innerHTML = "";
+
+          
+            notesData.forEach((noteText) => {
+                const note = document.createElement("div");
+                note.className = "note";
+
+                const textarea = document.createElement("textarea");
+                textarea.className = "note-textarea";
+                textarea.value = noteText;
+
+                const deleteButton = document.createElement("button");
+                deleteButton.className = "delete-button";
+                deleteButton.innerText = "Delete";
+
+                deleteButton.addEventListener("click", () => {
+                    noteContainer.removeChild(note);
+                    
+                    removeNoteFromArray({ textarea, note });
+                    
+                    removeNoteFromLocalStorage(textarea);
+                });
+
+                
+                noteContainer.appendChild(note);
+
+                note.appendChild(textarea);
+                note.appendChild(deleteButton);
+
+                
+                notesArray.unshift({ textarea, note });
+            });
+        }
+    }
+
+    function removeNoteFromArray(noteData) {
+        
+        notesArray = notesArray.filter(data => data !== noteData);
+    }
+
+    function removeNoteFromLocalStorage(textarea) {
+        const index = notesArray.findIndex(data => data.textarea === textarea);
+        if (index !== -1) {
+           
+            notesArray.splice(index, 1);
+            localStorage.setItem("notes", JSON.stringify(notesArray.map(noteData => noteData.textarea.value)));
+        }
+    }
+});
